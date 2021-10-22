c086184205eb114c3a1cab30dcdfa223bee2185c
Safin-Ahmed <safin.ahmed2000@gmail.com>
2021-10-23 02:19:10 +0600

    Initial Repo

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router';
 
 const UpdateUser = () => {
+    const[user, setUser] = setUser({});
+    const {id} = useParams();
+    useEffect(() => {
+        const url = `http://localhost:5000/users/${id}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setUser(data));
+    }, [])
     return (
         <div>
-            <h2>This is Update User</h2>
+            <h2>update {user.name}</h2>
+            <p><small>{id}</small></p>
         </div>
     );
 };
